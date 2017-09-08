410548d1d135498bd9d4758f6e739cb88c1e04aa
Bartek Krzys <b.krzys@clearcode.cc>
2017-09-09 00:46:58 +0200

    Created event model

---
+++ backend/code/archipelag/event/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+CATEGORIES = (
+    ('FB', 'Facebook'),
+    ('IP', 'Informacja Prasowa'),
+    ('NEWS', 'Newsletter'),
+    ('INST', 'Instagram'),
+    ('TW', 'Twitter'),
+    ('SMS', 'SMS Kiss'),
+)
+
+class EventModel(models.Model):
+    title  = models.CharField(max_length=120, blank=True, null=False)
+    email = models.EmailField(null=False)
+    category = models.CharField(max_length=4, choices=CATEGORIES, default='FB')
+    url = models.URLField(null=True)
+    date_starting = models.DateField()
+    date_ending = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
