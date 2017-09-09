3695c74448af662fb5333c8cb93b5264349e5200
Bartek Krzys <b.krzys@clearcode.cc>
2017-09-09 06:20:10 +0200

    Removed CATEGORy from Event

--- backend/code/archipelag/event/models.py
+++ backend/code/archipelag/event/models.py
@@ -2,18 +2,9 @@ from django.db import models
 from archipelag.ngo.models import NgoUser
 # Create your models here.
 
-CATEGORIES = (
-    ('FB', 'Facebook'),
-    ('IP', 'Informacja Prasowa'),
-    ('NEWS', 'Newsletter'),
-    ('INST', 'Instagram'),
-    ('TW', 'Twitter'),
-    ('SMS', 'SMS Kiss'),
-)
 
 class Event(models.Model):
     title  = models.CharField(max_length=120, blank=True, null=False)
-    category = models.CharField(max_length=4, choices=CATEGORIES, default='FB')
     url = models.URLField(null=True)
     date_starting = models.DateField()
     date_ending = models.DateField()
