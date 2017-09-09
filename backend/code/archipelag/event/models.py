5a6b6afcf9ec8a28bfaa88b401d1f68360d4a521
noemiko <noemikow8@gmail.com>
2017-09-09 03:49:13 +0200

    change model Shared to Market

--- backend/code/archipelag/event/models.py
+++ backend/code/archipelag/event/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from archipelag.ngo.models import NgoUser
 # Create your models here.
 
-CATEGORIES = (
-    ('FB', 'Facebook'),
-    ('IP', 'Informacja Prasowa'),
-    ('NEWS', 'Newsletter'),
-    ('INST', 'Instagram'),
-    ('TW', 'Twitter'),
-    ('SMS', 'SMS Kiss'),
-)
 
 class Event(models.Model):
     title = models.CharField(max_length=120, blank=True, null=False)
@@ -21,9 +13,3 @@ class Event(models.Model):
     shares = models.PositiveIntegerField()
     owner = models.ForeignKey(NgoUser)
 
-
-class Shared(models.Model):
-    category = models.CharField(max_length=4, choices=CATEGORIES, default='FB')
-    date_shared = models.DateField(null=False)
-    who_shared = models.ForeignKey(NgoUser)
-    hash_tag = models.CharField(max_length=120, null=False)
--- backend/code/archipelag/market/models.py
+++ backend/code/archipelag/market/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from archipelag.ngo.models import NgoUser
+from archipelag.event.models import Event
 # Create your models here.
+
+CATEGORIES = (
+    ('FB', 'Facebook'),
+    ('IP', 'Informacja Prasowa'),
+    ('NEWS', 'Newsletter'),
+    ('INST', 'Instagram'),
+    ('TW', 'Twitter'),
+    ('SMS', 'SMS Kiss'),
+)
+
+
+class Market(models.Model):
+    category = models.CharField(max_length=4, choices=CATEGORIES, default='FB')
+    date_shared = models.DateField(null=False)
+    who_shared = models.ForeignKey(NgoUser)
+    hash_tag = models.CharField(max_length=120, null=False)
+    event = models.ForeignKey(Event)
