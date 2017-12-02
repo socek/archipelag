12d14d0f276ba776b33c863b117ef95c1a701004
Firemark <marpiechula@gmail.com>
2017-12-03 00:19:57 +0100

    add helper scripts

---
+++ backend/code/archipelag/first-build-db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+bash migrate-and-run.sh
+
