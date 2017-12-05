79d59984d6ae71e0636863a16558cfce9fe55211
Marek Piechula <m.piechula@clearcode.cc>
2017-12-05 20:07:09 +0100

    fix scripts

--- backend/code/archipelag/first-build-db.sh
+++ backend/code/archipelag/first-build-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python manage.py migrate
+set -e
+python manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 echo 'Done!'
 exit 0
