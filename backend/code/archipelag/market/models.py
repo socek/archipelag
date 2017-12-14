b65ea3cc3458d99e5e756b769aa0691800ad2a5e
n.kowalewska <n.kowalewska@clearcode.cc>
2017-12-14 23:06:20 +0100

    delete points if user add market, refuse if hasnt coints

--- backend/code/archipelag/market/models.py
+++ backend/code/archipelag/market/models.py
@@ -8,6 +8,13 @@ from django.db.models import URLField
 
 from archipelag.ngo.models import NgoUser
 
+POINTS_RULES = dict(
+    for_share=2,
+    share_more_than_three=0.5,
+    create_more_than_three_messages_format=0.5,
+    share_more_than_three_messages_format=0.5,
+    add_own_market=3)
+
 
 class Market(Model):
     owner = ForeignKey(NgoUser)
--- backend/code/archipelag/market/views.py
+++ backend/code/archipelag/market/views.py
@@ -4,16 +4,11 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from json import loads
 from archipelag.market.models import Market
+from archipelag.market.models import POINTS_RULES
 from archipelag.message.models import Message
 from django.http import JsonResponse
 from django.shortcuts import redirect
-
-
-POINTS_RULES = dict(
-    for_share=2,
-    share_more_than_three=0.5,
-    create_more_than_three_messages_format=0.5,
-    share_more_than_three_messages_format=0.5,)
+from archipelag.ngo.models import NgoUser
 
 
 class MarketView(LoginRequiredMixin, View):
@@ -32,18 +27,33 @@ def market_create(request):
     if request.method == 'POST':
         body_unicode = request.body.decode('utf-8')
         body_data = loads(body_unicode)
-
-        new_market = Market.objects.create(
-            owner=request.user.ngouser,
-            title=body_data["title"],
-            url=body_data["url"],
-            date_starting=body_data["date_starting"],
-            date_ending=body_data["date_ending"],
-            hashtag = body_data["hashtag"]
-        )
-        data = dict(url='/message/create/%s'%new_market.id)
-        return JsonResponse(data)
+        current_ngo = request.user.ngouser
+        if can_create_market(current_ngo):
+            delete_points(current_ngo)
+            new_market = Market.objects.create(
+                owner=request.user.ngouser,
+                title=body_data["title"],
+                url=body_data["url"],
+                date_starting=body_data["date_starting"],
+                date_ending=body_data["date_ending"],
+                hashtag=body_data["hashtag"]
+            )
+            data = dict(url='/message/create/%s'%new_market.id)
+            return JsonResponse(data)
+        else:
+            error = dict(error="Za mało punktów.")
+            return JsonResponse(error)
     return render(request, 'registration/event.html', )
+
+
+def can_create_market(current_ngo):
+    ngo = NgoUser()
+    return ngo.is_user_can_add_market(current_ngo)
+
+
+def delete_points(current_ngo):
+    ngo = NgoUser()
+    ngo.subtract_coins(current_ngo, POINTS_RULES['add_own_market'])
 
 
 def get_messages(request, market_id):
@@ -57,7 +67,6 @@ def get_messages(request, market_id):
 
 
 def add_point(request, market_id):
-    from archipelag.ngo.models import NgoUser
     current_ngo = request.user.ngouser
     ngo_model = NgoUser()
     ngo_model.add_coins(current_ngo, POINTS_RULES['for_share'])
